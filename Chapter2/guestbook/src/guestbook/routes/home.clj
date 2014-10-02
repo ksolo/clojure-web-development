6389473f12b342dd998a054e35ede16aa74e7383
Kevin Solorio <kevin@detone8.com>
2014-10-02 13:36:59 -0400

    disply user from session

--- Chapter2/guestbook/src/guestbook/routes/home.clj
+++ Chapter2/guestbook/src/guestbook/routes/home.clj
@@ -2,7 +2,8 @@
   (:require [compojure.core :refer :all]
             [guestbook.views.layout :as layout]
             [hiccup.form :refer :all]
-            [guestbook.models.db :as db]))
+            [guestbook.models.db :as db]
+            [noir.session :as session]))
 
 (defn format-time [timestamp]
   (-> "dd/MM/yyyy"
@@ -19,7 +20,7 @@
 
 (defn home [& [name message error]]
   (layout/common
-    [:h1 "Guestbook"]
+    [:h1 "Guestbook " (session/get :user)]
     [:p "Welcome to my guestbook"]
     [:p error]
 
@@ -32,11 +33,8 @@
     ;these will be sent when the form posts to the server as keywords of
     ;the same name
     (form-to [:post "/"]
-      [:p "Name:"]
-      (text-field "name" name)
-      [:p "Message:"]
-      (text-area {:rows 10 :cols 40} "message" message)
-      [:br]
+      [:p "Name:" (text-field "name" name)]
+      [:p "Message:" (text-area {:rows 10 :cols 40} "message" message)]
       (submit-button "comment"))))
 
 (defn save-message [name message]
