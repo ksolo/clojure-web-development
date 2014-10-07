ff5ecbe3da5d309213b22543f3d46a07376cdd6d
Kevin Solorio <kevin@detone8.com>
2014-10-07 15:51:19 -0400

    updated reporting app with web interface

--- Chapter4/reporting-example/src/reporting_example/reports.clj
+++ Chapter4/reporting-example/src/reporting_example/reports.clj
@@ -22,3 +22,39 @@
            :header [{:color [0 150 150]} "Name" "Occupation" "Place" "Country"]}]
          (employee-template (db/read-employees)))]
   "report.pdf")
+
+(def employee-template-paragraph
+  (template
+    [:paragraph
+      [:heading {:style {:size 15}} $name]
+      [:chunk {:style :bold} "occupation: "] $occupation "\n"
+      [:chunk {:style :bold} "place: "] $place "\n"
+      [:chunk {:style :bold} "country: "] $country
+      [:spacer]]))
+
+(pdf
+  [{}
+    [:heading {:size 10} "Employees"]
+    [:line]
+    [:spacer]
+    (employee-template-paragraph (db/read-employees))]
+  "report.pdf")
+
+(defn table-report [out]
+  (pdf
+    [{:header "Employee List"}
+     (into [:table
+            {:border false
+             :cell-border false
+             :header [{:color [0 150 150]} "Name" "Occupation" "Place" "Country"]}]
+            (employee-template (db/read-employees)))]
+  out))
+
+(defn list-report [out]
+  (pdf
+    [{}
+      [:heading {:size 10} "Employees"]
+      [:line]
+      [:spacer]
+    (employee-template-paragraph (db/read-employees))]
+    out))
