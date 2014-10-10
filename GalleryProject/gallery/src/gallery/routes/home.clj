8b71387f093e8ccaaffa8fe6bc4c5f60e044cd67
Kevin Solorio <kevin@detone8.com>
2014-10-10 11:21:19 -0400

    use noir.session

--- GalleryProject/gallery/src/gallery/routes/home.clj
+++ GalleryProject/gallery/src/gallery/routes/home.clj
@@ -1,9 +1,10 @@
 (ns gallery.routes.home
   (:require [compojure.core :refer :all]
+            [noir.session :as session]
             [gallery.views.layout :as layout]))
 
 (defn home []
-  (layout/common [:h1 "Hello World!"]))
+  (layout/common [:h1 "Hello " (session/get :user)]))
 
 (defroutes home-routes
   (GET "/" [] (home)))
