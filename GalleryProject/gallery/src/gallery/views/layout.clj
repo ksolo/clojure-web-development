73c6c542a4302be6fa5e4be01ab9ba37b86720ce
Kevin Solorio <kevin@detone8.com>
2014-10-12 16:23:06 -0400

    added login/logout

--- GalleryProject/gallery/src/gallery/views/layout.clj
+++ GalleryProject/gallery/src/gallery/views/layout.clj
@@ -1,6 +1,7 @@
 (ns gallery.views.layout
   (:require [hiccup.page :refer [html5 include-css]]
             [hiccup.element :refer [link-to]]
+            [hiccup.form :refer :all]
             [noir.session :as session]))
 
 (defn base [& content]
@@ -13,5 +14,10 @@
 (defn common [& content]
   (base
     (if-let [user (session/get :user)]
-      [:p user] (link-to "/register" "register"))
+      [:div (link-to "/logout" (str "logout" user))]
+      [:div (link-to "register" "register")
+        (form-to [:post "/login"]
+          (text-field {:placeholder "screen name"} id)
+          (password-field {:placeholder "password"} pass)
+          (submit-button "login"))]
     content))
