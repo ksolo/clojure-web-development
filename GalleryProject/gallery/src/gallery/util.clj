8b542a9aa683877a25c42ba5d913f062a9d4f009
Kevin Solorio <kevin@detone8.com>
2014-10-19 12:00:03 -0400

    updated gallery.util ns

--- GalleryProject/gallery/src/gallery/util.clj
+++ GalleryProject/gallery/src/gallery/util.clj
@@ -1,8 +1,16 @@
 (ns gallery.util
-  (:require [noir.session :as session])
+  (:require [noir.session :as session]
+            [hiccup.util :refer [url-encode]])
   (:import java.io.File))
 
+(def thumb-prefix "thumb_")
 (def gallery "galleries")
 
 (defn gallery-path []
   (str gallery File/separator (session/get :user)))
+
+(defn image-uri [userid file-name]
+  (str "/img/" userid "/" (url-encoe file-name))
+
+(defn thumb-uri [userid file-name]
+  (image-uri userid (str thumb-prefix file-name)))
