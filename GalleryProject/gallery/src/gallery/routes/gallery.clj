3d76d3245ecc62c4bc6ddb106d668cfff7840d98
Kevin Solorio <kevin@detone8.com>
2014-10-22 12:13:04 -0400

    set up for ajax deletion

--- GalleryProject/gallery/src/gallery/routes/gallery.clj
+++ GalleryProject/gallery/src/gallery/routes/gallery.clj
@@ -1,6 +1,8 @@
 (ns gallery.routes.gallery
   (:require [compojure.core :refer :all]
             [hiccup.element :refer :all]
+            [hiccup.form :refer [check-box]]
+            [hiccup.page :refer :all]
             [gallery.views.layout :as layout]
             [gallery.util :refer [thumb-prefix image-uri thumb-uri]]
             [gallery.models.db :as db]
@@ -8,8 +10,9 @@
 
 (defn thumbnail-link [{:keys [userid name]}]
   [:div.thumbnail
-    [:a {:href (image-uri userid name)}
-      (image (thumb-uri userid name))]])
+    [:a {:href (image-uri userid name) :class name}
+      (image (thumb-uri userid name))
+      (if (= userid (session/get :user)) (check-box name))]])
 
 (defn gallery-link [{:keys [userid name]}]
   [:div.thumbnail
@@ -21,9 +24,16 @@
   (map gallery-link (db/get-gallery-previews)))
 
 (defn display-gallery [userid]
-  (or
-    (not-empty (map thumbnail-link (db/images-by-user userid)))
+  (if-let [gallery (not-empty (map thumbnail-link (db/images-by-user userid)))]
+    [:div
+      [:div#error]
+      gallery
+      (if (= userid (session/get :user))
+        [:input#delete {:type "submit" :value "delete images"}])]
     [:p "The user " userid " does not have any galleries"]))
 
 (defroutes gallery-routes
-  (GET "/gallery/:userid" [userid] (layout/common (display-gallery userid))))
+  (GET "/gallery/:userid" [userid]
+    (layout/common
+      (include-js "/js/gallery.js")
+      (display-gallery userid))))
